fd0759e4736aee3c3c3f6c91fdef053ffbdc09af
Ben Ford <ben.ford@puppetlabs.com>
2015-12-10 08:53:04 -0800

    Add a reconnect button on logout
    Prior to this, the only way to return to a session after logging out or
    login timeout was to manually reload the page. This gives you a nice
    button to do it for you.
    
    Ping @joshsamuelson

--- app.js
+++ app.js
@@ -120,7 +120,8 @@ io.on('connection', function(socket){
         socket.emit('output', data);
     });
     term.on('exit', function(code) {
-        console.log((new Date()) + " PID=" + term.pid + " ENDED")
+        console.log((new Date()) + " PID=" + term.pid + " ENDED");
+        socket.emit('logout');
     });
     socket.on('resize', function(data) {
         term.resize(data.col, data.row);
