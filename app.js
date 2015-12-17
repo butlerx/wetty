99dfb3d2d243637ad973b7358da68a71ad9b41a9
Tino Rusch <tino.rusch@webvariants.de>
2015-12-17 14:24:36 +0100

    allways use ssh and never /bin/login;

--- app.js
+++ app.js
@@ -117,19 +117,13 @@ io.on('connection', function(socket){
     }
 
     var term;
-    if (process.getuid() === 0) {
-        term = pty.spawn('/bin/login', [], {
-            name: 'xterm-256color',
-            cols: 80,
-            rows: 30
-        });
-    } else {
-        term = pty.spawn('ssh', [sshuser + sshhost, '-p', sshport, '-o', 'PreferredAuthentications=' + sshauth], {
-            name: 'xterm-256color',
-            cols: 80,
-            rows: 30
-        });
-    }
+
+    term = pty.spawn('ssh', [sshuser + sshhost, '-p', sshport, '-o', 'PreferredAuthentications=' + sshauth], {
+        name: 'xterm-256color',
+        cols: 80,
+        rows: 30
+    });
+
     console.log((new Date()) + " PID=" + term.pid + " STARTED on behalf of user=" + sshuser);
     term.on('data', function(data) {
         socket.emit('output', data);
