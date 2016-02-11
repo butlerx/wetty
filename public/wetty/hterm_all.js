ffd250a3db2ff4ed412a3dbf93d4be77e6a43a82
Antonio Calatrava <antonio@antoniocalatrava.com>
2016-02-11 22:24:08 +0100

    Add support for Virtual Keyboard on Mobile Devices

--- public/wetty/hterm_all.js
+++ public/wetty/hterm_all.js
@@ -8375,6 +8375,7 @@ hterm.ScrollPort.prototype.decorate = function(div) {
   // from screen.js.  I need to pick a better name for one of them to avoid
   // the collision.
   this.screen_ = doc.createElement('x-screen');
+  this.screen_.setAttribute('contenteditable', '');
   this.screen_.setAttribute('role', 'textbox');
   this.screen_.setAttribute('tabindex', '-1');
   this.screen_.style.cssText = (
