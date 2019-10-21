d1584354d53b67cf39f0e659ae9fc92cbdc82f90
butlerx <butlerx@notthe.cloud>
2019-10-21 20:46:05 +0100

    Fix #202, check if ssl is defined

--- src/server/ssl.ts
+++ src/server/ssl.ts
@@ -4,7 +4,7 @@ import { isUndefined } from 'lodash';
 import { SSL, SSLBuffer } from './interfaces';
 
 export default async function loadSSL(ssl: SSL): Promise<SSLBuffer> {
-  if (isUndefined(ssl.key) || isUndefined(ssl.cert)) return {};
+  if (isUndefined(ssl) || isUndefined(ssl.key) || isUndefined(ssl.cert)) return {};
   const files = [readFile(resolve(ssl.key)), readFile(resolve(ssl.cert))];
   const [key, cert]: Buffer[] = await Promise.all(files);
   return { key, cert };
