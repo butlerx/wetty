1e4f173196860ee1570fe59411a9feff238b0720
Alex Cline <rev.a.r.cline@gmail.com>
2022-08-02 02:38:43 -0500

    escape special chars in username: `- . \ @` (#406)

--- src/server/shared/shell.spec.ts
+++ src/server/shared/shell.spec.ts
@@ -8,6 +8,11 @@ describe('Values passed to escapeShell should be safe to pass woth sub processes
     expect(cmd).to.equal('testechohello');
   });
 
+  it('should allow usernames with special characters', () => {
+    const cmd = escapeShell('bob.jones\\COM@ultra-machine_dir');
+    expect(cmd).to.equal('bob.jones\\COM@ultra-machine_dir');
+  });
+
   it('should ensure args cant be flags', () => {
     const cmd = escapeShell("-oProxyCommand='bash' -c `wget localhost:2222`");
     expect(cmd).to.equal('oProxyCommandbash-cwgetlocalhost2222');
--- src/server/shared/shell.ts
+++ src/server/shared/shell.ts
@@ -1,2 +1,3 @@
 export const escapeShell = (username: string): string =>
-  username.replace(/^-|[^a-zA-Z0-9_-]/g, '');
+  // eslint-disable-next-line no-useless-escape
+  username.replace(/^-|[^a-zA-Z0-9_\\\-\.\@-]/g, '');
