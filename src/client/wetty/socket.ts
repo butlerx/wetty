f6d580767c76e039c7385a33b23415bf03e9437c
Warr1024 <warr1024@gmail.com>
2022-09-11 14:01:11 -0400

    Fix socket.io failure to connect when using wetty/ssh/user paths

--- src/client/wetty/socket.ts
+++ src/client/wetty/socket.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 export const trim = (str: string): string => str.replace(/\/*$/, '');
 
-const socketBase = trim(window.location.pathname).replace('/ssh/[^/]+$/', '');
+const socketBase = trim(window.location.pathname).replace(/ssh\/[^/]+$/, '');
 export const socket = io(window.location.origin, {
   path: `${trim(socketBase)}/socket.io`,
 });
