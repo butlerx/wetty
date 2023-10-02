cdac5b9b0216ef4859e87b5a6c3b0b4319562cef
Cian Butler <butlerx@notthe.cloud>
2023-10-02 21:31:50 +0100

    fix type error

--- src/server/socketServer/assets.ts
+++ src/server/socketServer/assets.ts
@@ -1,5 +1,5 @@
-import express from 'express';
+import serve from 'serve-static';
 import { assetsPath } from './shared/path.js';
 
 export const trim = (str: string): string => str.replace(/\/*$/, '');
-export const serveStatic = (path: string) => express.static(assetsPath(path));
+export const serveStatic = (path: string) => serve(assetsPath(path));
